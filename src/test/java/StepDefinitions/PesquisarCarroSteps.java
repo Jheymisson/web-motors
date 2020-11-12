93763f9e37f02745368e1eb45111a333dea77333
jheymix <35575753+Jheymisson@users.noreply.github.com>
2020-11-12 17:32:14 -0300

    Finalização do step PesquisarCarroSteps

--- src/test/java/StepDefinitions/PesquisarCarroSteps.java
+++ src/test/java/StepDefinitions/PesquisarCarroSteps.java
@@ -2,6 +2,10 @@ package StepDefinitions;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 
 import Core.BaseTest;
@@ -40,37 +44,79 @@ public class PesquisarCarroSteps {
 
 	@Dado("preencher o campo de pesquisa com uma marca ou modelo")
 	public void preencher_o_campo_de_pesquisa_com_uma_marca_ou_modelo() {
-
+		new POPesquisarCarro(driver)
+			.selecionarAbaComprarCarros()
+			.digitarMarca("HONDA");
 	}
 
 	@Quando("selecionar opção com a marca preenchida no campo")
 	public void selecionar_opção_com_a_marca_preenchida_no_campo() {
-
+		new POPesquisarCarro(driver)
+			.selecionarOpcaoDropdownDaPesquisa();
 	}
 
 	@Entao("irá apareccer uma nova tela com resultados obtidos através da pesquisa")
 	public void irá_apareccer_uma_nova_tela_com_resultados_obtidos_através_da_pesquisa() {
-
+		String marcaModelo =
+		new ResultadosPesquisa(driver)
+			.validarMarcaModelo();
+		assertEquals(marcaModelo, "HONDA");
 	}
 
 	@Dado("entrar na página de resultados da pesquisa")
 	public void entrar_na_página_de_resultados_da_pesquisa() {
-
+		new POPesquisarCarro(driver)
+			.selecionarAbaComprarCarros()
+			.digitarMarca("HONDA")
+			.selecionarOpcaoDropdownDaPesquisa();
+		String marca =
+		new ResultadosPesquisa(driver)
+			.validarMarcaModelo();
+		assertEquals(marca, "HONDA");
 	}
 
 	@Dado("selecionar modelo de um carro")
 	public void selecionar_modelo_de_um_carro() {
-
+		new Reutilizavel(driver)
+			.scrollFrame();
+		new ResultadosPesquisa(driver)
+			.selecionarBotaoModelos()
+			.selecionarOpcaoModelo();
 	}
 
 	@Entao("modelo será selecionado com sucesso")
 	public void modelo_será_selecionado_com_sucesso() {
-
+		String marcaModelo =
+		new ResultadosPesquisa(driver)
+			.validarMarcaModelo();
+		assertEquals(marcaModelo, "HONDA CITY");
 	}
 
 	@Entao("resultados serao exportados para arquivo cvs com sucesso")
 	public void resultados_serao_exportados_para_arquivo_cvs_com_sucesso() {
-
+		ResultadosPesquisa resultados = new ResultadosPesquisa(this.driver);
+		List<String> listaMarcaModelo = resultados.listaDeMarcaModelo();
+		List<String> listaEspecificacoes = resultados.listaDeEspecificacoes();
+		List<String> listaPrecos = resultados.listaDePreco();
+		File f = new File(".\\dados.csv");
+		try {
+			FileWriter fw = new FileWriter(f);
+			fw.write("Marca e Modelo; Especificacoes; Precos");
+			fw.write("\n");
+			for(int i=0;i < listaMarcaModelo.size(); i++){
+				String linha = 
+						listaMarcaModelo.get(i) + ";" 
+						+ listaEspecificacoes.get(i) + ";"
+						+ listaPrecos.get(i);
+				fw.write(linha);
+				fw.write("\n");
+			}
+			fw.flush();
+			fw.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Erro ao criar arquivo");
+		}
 	}
 	
 	@After
